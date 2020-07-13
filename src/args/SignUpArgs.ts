319f7b0a37b03d2975f98a2a9ab76f370a3d4806
Dan Beneventano <danbeneventano@protonmail.com>
2020-07-13 15:46:33 -0400

    Add IsOptional() to email

--- src/args/SignUpArgs.ts
+++ src/args/SignUpArgs.ts
@@ -1,10 +1,11 @@
 import { ArgsType, Field } from 'type-graphql'
-import { IsEmail } from 'class-validator'
+import { IsEmail, IsOptional } from 'class-validator'
 import { LoginArgs } from './LoginArgs'
 
 @ArgsType()
 export class SignUpArgs extends LoginArgs {
   @Field({ nullable: true })
+  @IsOptional()
   @IsEmail()
   email?: string
 }
