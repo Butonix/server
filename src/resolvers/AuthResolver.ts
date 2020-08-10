fbc9d07212fe868f13aa796e879356f91b09f269
Dan Beneventano <danbeneventano@protonmail.com>
2020-08-10 00:19:25 -0400

    Update inappropriate names

--- src/resolvers/AuthResolver.ts
+++ src/resolvers/AuthResolver.ts
@@ -34,12 +34,26 @@ export class AuthResolver {
       'c00n',
       'faggot',
       'fagg0t',
+      'fag',
       'whitepower',
+      'white_power',
+      'white-power',
+      'blackpower',
+      'black_power',
+      'black-power',
       'cunt',
       'negro',
       'n3gro',
       'negr0',
-      'n3gr0'
+      'n3gr0',
+      'cracker',
+      'crack3r',
+      'chink',
+      'beaner',
+      'bean3r',
+      'b3aner',
+      'b3an3r',
+      'spic'
     ]
 
     inappropiateUsernames.forEach((u) => {
