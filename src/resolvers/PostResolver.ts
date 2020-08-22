ab8f00b938d6266aa0537b0e1310f95f1dde1f7d
Dan Beneventano <danbeneventano@protonmail.com>
2020-08-22 17:19:17 -0600

    Temporarily remove stickies

--- src/resolvers/PostResolver.ts
+++ src/resolvers/PostResolver.ts
@@ -245,7 +245,7 @@ export class PostResolver extends RepositoryInjector {
       .loadRelationCountAndMap('planet.userCount', 'planet.users')
       .getMany()
 
-    if (planetName && page === 0) {
+    /*if (planetName && page === 0) {
       const stickiesQb = await this.postRepository
         .createQueryBuilder('post')
         .andWhere('post.sticky = true')
@@ -281,9 +281,8 @@ export class PostResolver extends RepositoryInjector {
 
       const stickies = await stickiesQb.getMany()
 
-      console.log(stickies)
       posts = stickies.concat(posts)
-    }
+    }*/
 
     for (const p of posts) {
       p.isEndorsed = Boolean(p.personalEndorsementCount)
